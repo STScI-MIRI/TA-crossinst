abdc4a1e14c6ec3177f0af96070fed133f788b67
Sarah Kendrew <sarahaskendrew@gmail.com>
2018-05-10 13:37:20 -0400

    nircam test file update for new filenames and centroid calling sequence

--- nircam_test.py
+++ nircam_test.py
@@ -12,11 +12,11 @@ from jwst_ta import centroid
 
 #files = glob.glob('*.fits')
 #files = ['nrca5_TA_timeseries_NRCFLATA5GRTS_mag09.4_center_15.5_15.5_noiserealiz_1_uncal.fits']
-files = ['nrca5_TA_timeseries_NRCFLATA5GRTS_mag10.0_center_15.5_15.5_noiserealiz_1_uncal.fits']
+files = ['nrca5_TA_timeseries_NRCFLATA5GRTS_mag09.4_center_15.25_15.25_noiserealiz_1_uncal.fits', 'nrca5_TA_timeseries_NRCFLATA5GRTS_mag09.4_center_15.75_15.75_noiserealiz_1_uncal.fits']
 #flatfile = 'NRCFLATA5GRTS_flat.fits'
 flatfile = 'NRCFLATA5GRTS_flat_withbadpix.fits'
 
-for file in files[:1]:
+for file in files:
     print(file)
     with fits.open(file) as h:
         yd, xd = h[1].data.shape[-2:]
@@ -26,5 +26,4 @@ for file in files[:1]:
     # is 9x9. Making the checkbox larger (e.g. 5x5) leads to very
     # poor centroiding results.
     x,y = centroid(infile=file, input_type='ramp', ext=1,
-                   cbox=3, cwin=9, incoord=(xin, yin), bgcorr=-1,
-                   flat=flatfile)
+                   cbox=3, cwin=9, incoord=(xin, yin), bgcorr=-1, thresh=0.5)
