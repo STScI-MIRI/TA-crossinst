fixed appending bad pixels issue and made silencing more silent

jwst_ta/jwst_ta.py
@@ -218,7 +218,7 @@ def centroid_from_image(
                                 "not match data shape ({})!"
                                 .format(ffshape,dshape)))
         # Apply flat
-        im = apply_flat_field(im, flatfield)
+        im = apply_flat_field(im, flatfield, silent=silent)
         
     ndim = np.ndim(im)
     
@@ -494,7 +494,7 @@ def make_ta_image(infile, ext=0, useframes=3, save=False, silent=False):
 
 
 #=====================================================
-def apply_flat_field(image, flat):
+def apply_flat_field(image, flat, silent=False):
     """
     Apply flat field to TA image. Assume the flat 
     has the format matching those to be used on 
@@ -529,14 +529,14 @@ def apply_flat_field(image, flat):
     # NOT SURE IF THIS IS IMPLEMENTED IN THE REAL
     # GENTALOCATE OR NOT...
     if np.any(bad):
-        image = fixbadpix(image)
+        image = fixbadpix(image, silent=silent)
 
     return image
 
 
 
 #======================================================
-def fixbadpix(data, maxstampwidth=3, method='median'):
+def fixbadpix(data, maxstampwidth=3, method='median', silent=False):
     """
     Replace the values of bad pixels in the TA image
     with interpolated values from neighboring good
@@ -581,7 +581,8 @@ def fixbadpix(data, maxstampwidth=3, method='median'):
     # Loop over the bad pixels and correct
     for bady, badx in zip(bad[0], bad[1]):
 
-        print('Bad pixel:',bady,badx)
+        if not silent:
+            print('Bad pixel:',bady,badx)
         
         substamp = np.zeros((maxstampwidth, maxstampwidth))
         substamp[:,:] = np.nan
@@ -621,18 +622,21 @@ def fixbadpix(data, maxstampwidth=3, method='median'):
         neighborsy = [half+1, half, half-1, half]
         if np.sum(np.isnan(substamp[neighborsx, neighborsy])) < 4:
             data[bady, badx] = mmethod(substamp[neighborsx, neighborsy])
-            print(("Good pixels within nearest 4 neighbors. Mean: {}"
-                   .format(mmethod(substamp[neighborsx, neighborsy]))))
+            if not silent:
+                print(("Good pixels within nearest 4 neighbors. Mean: {}"
+                       .format(mmethod(substamp[neighborsx, neighborsy]))))
             continue
 
         # If the adjacent pixels are all NaN, expand to include corners
         else:
-            neighborsx.append([half-1, half+1, half+1, half-1])
-            neighborsy.append([half+1, half+1, half-1, half-1])
+            for i in [half-1, half+1, half+1, half-1]:
+                neighborsx.append(i)
+                neighborsy.append(i)
             if np.sum(np.isnan(substamp[neighborsx, neighborsy])) < 8:
                 data[bady, badx] = mmethod(substamp[neighborsx, neighborsy])
-                print(("Good pixels within 8 nearest neighbors. Mean: {}"
-                       .format(mmethod(substamp[neighborsx, neighborsy]))))
+                if not silent:
+                    print(("Good pixels within 8 nearest neighbors. Mean: {}"
+                           .format(mmethod(substamp[neighborsx, neighborsy]))))
                 continue
 
         # If all pixels are still NaN, iteratviely expand to include
@@ -657,13 +661,15 @@ def fixbadpix(data, maxstampwidth=3, method='median'):
             neighborsy.extend(newy)
             if np.sum(np.isnan(substamp[neighborsx, neighborsy])) < (len(neighbosrsx)):
                 data[bady, badx] = mmethod(substamp[neighborsx, neighborsy])
-                print("Expanding to {} rows".format(delta))
+                if not silent:
+                    print("Expanding to {} rows".format(delta))
                 continue
             else:
                 delta += 1
-        print(("Warning: all pixels within {} rows/cols of the bad pixel at ({},{}) "
-               "are also bad. Cannot correct this bad pixel with this stamp image"
-               "size.".format(delta, badx, bady)))
+        if not silent:
+            print(("Warning: all pixels within {} rows/cols of the bad pixel at ({},{}) "
+                   "are also bad. Cannot correct this bad pixel with this stamp image"
+                   "size.".format(delta, badx, bady)))
 
     return data
 